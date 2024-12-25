dcd80b1e243894470876d7ef2ed9e3204fba8a7d
Aquademon <98233916+Aquademon@users.noreply.github.com>
2024-12-25 20:48:12 +0300

    Update Command.java

--- src/main/java/dev/aquademon/interfaces/Command.java
+++ src/main/java/dev/aquademon/interfaces/Command.java
@@ -1,9 +1,12 @@
 package dev.aquademon.interfaces;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import java.util.List;
 
 public interface Command {
     void execute(SlashCommandInteractionEvent event);
     String getName();
     String getDescription();
+    List<OptionData> getOptions();
 }
