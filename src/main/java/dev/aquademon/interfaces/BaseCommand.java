f8135a49f3b09cb44dbecf90d5596e6adfc94cc1
Aquademon <98233916+Aquademon@users.noreply.github.com>
2024-12-25 20:47:54 +0300

    Update BaseCommand.java

--- src/main/java/dev/aquademon/interfaces/BaseCommand.java
+++ src/main/java/dev/aquademon/interfaces/BaseCommand.java
@@ -1,12 +1,18 @@
 package dev.aquademon.interfaces;
 
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class BaseCommand implements Command {
     private final String name;
     private final String description;
+    private final List<OptionData> options;
 
     public BaseCommand(String name, String description) {
         this.name = name;
         this.description = description;
+        this.options = new ArrayList<>();
     }
 
     @Override
@@ -17,5 +23,14 @@ public abstract class BaseCommand implements Command {
     @Override
     public String getDescription() {
         return description;
+    }
+
+    @Override
+    public List<OptionData> getOptions() {
+        return options;
+    }
+
+    protected void addOption(OptionData option) {
+        options.add(option);
     }
 }
